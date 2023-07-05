14efb66e691017a78473e1defcbec719370d4704
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-07-05 07:45:18 -0300

    add subpoeddit slug, add sql query to find subpoeddit

--- app/p/[slug]/page.tsx
+++ app/p/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import { findSubpoedditByNameIncludePosts } from '@/db/subPoeddit'
+import { getAuthSession } from '@/lib/auth'
+import { notFound } from 'next/navigation'
 import React, { FC } from 'react'
 
 interface SubpoedditSlugPageProps {
@@ -5,11 +8,15 @@ interface SubpoedditSlugPageProps {
     slug: string
   }
 }
-const subpoedditSlugPage: FC<SubpoedditSlugPageProps> = ({ params }) => {
+const subpoedditSlugPage: FC<SubpoedditSlugPageProps> = async ({ params }) => {
   const { slug } = params
+  const session = await getAuthSession();
+  const subpoeddit = await findSubpoedditByNameIncludePosts(slug)
+
+  if (!subpoeddit) return notFound()
   return (
     <div>
-      <h2>{slug}</h2>
+      <h2 className='font-bold text-3xl md:text-4xl h-14'>p/{subpoeddit.name}</h2>
     </div>
   )
 }
--- db/subPoeddit.ts
+++ db/subPoeddit.ts
@@ -1,9 +1,29 @@
+import { INFINITE_SCROLLING_PAGINATION } from "@/lib/constants";
 import { db } from "@/lib/prisma";
 
 export const findSubpoedditByName = async (name: string) => {
   return await db.subpoeddit.findFirst({
     where: {
       name,
+    },
+  });
+};
+
+export const findSubpoedditByNameIncludePosts = async (name: string) => {
+  return await db.subpoeddit.findFirst({
+    where: {
+      name,
+    },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          comments: true,
+          votes: true,
+          subpoeddit: true,
+        },
+        take: INFINITE_SCROLLING_PAGINATION,
+      },
     },
   });
 };
